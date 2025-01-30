2947ba3ce22cf4a2d8cae2b0f28bcdcd787b4a39
Shamsul Arifin <arifinbd6@gmail.com>
2025-01-30 16:55:03 +0600

    first commit

---
+++ Project/hello.cpp
@@ -0,0 +1,14 @@
+/*
+hello.cpp
+prints out my name
+*/
+
+#include <cstdio>
+
+int main()
+{
+    // Prints out my name
+    printf("Hello My Name is Arifin.\n");
+    printf("Hello My Name is Arifin.\n");
+    printf("Hello My Name is Arifin.");
+}
