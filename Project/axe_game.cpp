7adc9f1f89c056fb0f6c271331233132b3379c6b
Shamsul Arifin <arifinbd6@gmail.com>
2025-01-31 15:31:47 +0600

    Added some new variables

--- Project/axe_game.cpp
+++ Project/axe_game.cpp
@@ -2,10 +2,14 @@
 
 int main()
 {
-    float root_beer = 1.99;
-    double cheese_burger = 5.99;
-    bool shouldHaveLunch = true;
-    
-    int width = 350;
+    float root_beer;
+    root_beer = 1.99;
+    double cheese_burger;
+    cheese_burger = 5.99;
+    bool shouldHaveLunch;
+    shouldHaveLunch = true;
+
+    int width;
+    width = 350;
     InitWindow(width, 200, "Hello!");
 }
