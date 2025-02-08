42b79fa9486bb38b0338badef2799b5698713867
Shamsul Arifin <arifinbd6@gmail.com>
2025-02-08 20:41:51 +0600

    added collision

--- Project/axe_game.cpp
+++ Project/axe_game.cpp
@@ -11,10 +11,24 @@ int main()
 	// circle coordinates
 	int circle_x{200};
 	int circle_y{200};
+	int circle_radius{25};
+
+	// circle edges
+	int l_circle_x{circle_x - circle_radius};
+	int r_circle_x{circle_x + circle_radius};
+	int u_circle_y{circle_y - circle_radius};
+	int b_circle_y{circle_y + circle_radius};
 	
 	// axe coordinates
 	int axe_x{400};
 	int axe_y{0};
+	int axe_length{50};
+
+	// axe edges
+	int l_axe_x{axe_x};
+	int r_axe_x{axe_x + axe_length};
+	int u_axe_y{axe_y};
+	int b_axe_y{axe_y + axe_length};
 
 	int direction{10};
 
@@ -26,17 +40,17 @@ int main()
 
 		// Game logic begins
 
-		DrawCircle(circle_x, circle_y, 25, DARKGREEN);
-		DrawRectangle(axe_x, axe_y, 50, 50, RED);
+		DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+		DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
 		// move the axe
 		axe_y += direction;
-		if(axe_y > 450 || axe_y < 0)
+		if(axe_y > window_height || axe_y < 0)
 		{
 			direction = -direction;
 		}
 
-		if (IsKeyDown(KEY_D) && circle_x < 350)
+		if (IsKeyDown(KEY_D) && circle_x < window_width)
 		{
 			circle_x += 10;
 		}
