a77929fd17c354084c880b5f62586773248b6b6a
Shamsul Arifin <arifinbd6@gmail.com>
2025-02-08 12:04:00 +0600

    Clear background

--- Project/axe_game.cpp
+++ Project/axe_game.cpp
@@ -2,20 +2,14 @@
 
 int main()
 {
-    // false
-    bool equal{4 == 9};
-    // true
-    bool notEqual{ 4 != 9 };
-    // true
-    bool less{ 4 < 9 };
-    // false
-    bool greater{  4 > 9 };
-    // true
-    bool lessEqual{ 4 <= 9 };
-    // false
-    bool greaterEqual{ 4 >= 9 };
+	int width;
+	width = 350;
+	InitWindow(width, 200, "Sam's Window");
 
-    int width;
-    width = 350;
-    InitWindow(width, 200, "Hello!");
+	while (true)
+	{
+		BeginDrawing();
+		ClearBackground(RED);
+		EndDrawing();
+	}
 }
